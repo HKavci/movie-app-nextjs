81d9db0ba8495a7cbf9d4be984383c085d14f9d6
HakanKavci <hackankavci0@gmail.com>
2023-05-03 00:15:47 +0300

    done for now

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,21 @@
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+import {notFound} from "next/navigation"
+
+const MoviePage = ({ params, searchParams }) => {
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id === params.id
+  );
+
+  if (!movieDetail) {
+    notFound()
+  }
+
+  if (searchParams.error === "true") {
+    throw new Error("Error happened")
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+};
+
+export default MoviePage;
